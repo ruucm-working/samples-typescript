1a82b50ee8f4dfae61010a875b4801610ffe9423
Loren 🤓 <lorensr@gmail.com>
2022-05-24 23:31:53 -0400

    throw nonRetryable

--- hello-world/src/activities.ts
+++ hello-world/src/activities.ts
@@ -1,5 +1,8 @@
+import { ApplicationFailure } from '@temporalio/common';
+
 // @@@SNIPSTART typescript-hello-activity
 export async function greet(name: string): Promise<string> {
+  throw ApplicationFailure.nonRetryable('test nonretryable error');
   return `Hello, ${name}!`;
 }
 // @@@SNIPEND
