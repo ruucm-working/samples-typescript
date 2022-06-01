e8f88da250d59d20451f09f50cc0df0e8c2ae8e1
Loren 🤓 <lorensr@gmail.com>
2022-05-31 20:06:43 -0400

    Import @temporalio/client in Worker

--- hello-world/src/activities.ts
+++ hello-world/src/activities.ts
@@ -1,5 +1,8 @@
+import { WorkflowClient } from '@temporalio/client';
+
 // @@@SNIPSTART typescript-hello-activity
 export async function greet(name: string): Promise<string> {
-  return `Hello, ${name}!`;
+  const client = new WorkflowClient();
+  return `Hello, ${name + client}!`;
 }
 // @@@SNIPEND
