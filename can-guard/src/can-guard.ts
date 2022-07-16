a99b8f45a4c0ba4bc5c8952db9be4ffd41e1569c
Roey Berman <roey.berman@gmail.com>
2022-07-15 17:34:53 -0700

    Add CAN guard sample

---
+++ can-guard/src/can-guard.ts
@@ -0,0 +1,100 @@
+import * as wf from '@temporalio/workflow';
+import type { coresdk } from '@temporalio/proto';
+
+export type SignalArgs<Def extends wf.SignalDefinition> = Def extends wf.SignalDefinition<infer Args> ? Args : never;
+
+export interface SignalManagerConfig {
+  bufferSignalEventCountThreshold: number;
+  maxConcurrentSignalHandlers: number;
+}
+
+class SignalManager {
+  numRunningHandlers = 0;
+  numPendingHandlers = 0;
+  public readonly buffer = Array<wf.SignalInput>();
+
+  constructor(
+    public readonly config: SignalManagerConfig = {
+      bufferSignalEventCountThreshold: 1000,
+      maxConcurrentSignalHandlers: Infinity,
+    }
+  ) {}
+
+  async wrapSignalHandler(input: wf.SignalInput, next: wf.Next<wf.WorkflowInboundCallsInterceptor, 'handleSignal'>) {
+    this.numPendingHandlers++;
+    await wf.condition(() => this.numRunningHandlers < this.config.maxConcurrentSignalHandlers);
+    this.numPendingHandlers--;
+
+    if (wf.taskInfo().historyLength >= this.config.bufferSignalEventCountThreshold) {
+      this.buffer.push(input);
+    } else {
+      this.numRunningHandlers++;
+      try {
+        await next(input);
+      } finally {
+        this.numRunningHandlers--;
+      }
+    }
+  }
+
+  async readyToContinueAsNew() {
+    await wf.condition(() => this.numPendingHandlers === 0 && this.numRunningHandlers === 0);
+  }
+}
+
+const someSignal = wf.defineSignal<[number]>('something');
+
+export async function someInfiniteWorkflow(): Promise<void> {
+  wf.setHandler(someSignal, async (_x) => {
+    // run some activity or other async work here
+  });
+  // Workflow never completes - blocked by interceptor
+}
+
+export const interceptors: wf.WorkflowInterceptorsFactory = () => {
+  const bufferedSignals = (wf.workflowInfo().memo?.bufferedSignals ?? []) as wf.SignalInput[];
+  const sm = new SignalManager();
+  let activatedOnce = false;
+  return {
+    internals: [
+      {
+        activate(input, next) {
+          if (activatedOnce) return next(input);
+          activatedOnce = true;
+          const jobs = bufferedSignals.map(
+            (signal): coresdk.workflow_activation.IWorkflowActivationJob => ({
+              signalWorkflow: signal,
+            })
+          );
+          return next({
+            ...input,
+            activation: { ...input.activation, jobs: jobs.concat(input.activation.jobs ?? []) },
+          });
+        },
+      },
+    ],
+    outbound: [
+      {
+        async continueAsNew(input, next) {
+          await sm.readyToContinueAsNew();
+          return await next({
+            ...input,
+            options: { ...input.options, memo: { ...input.options.memo, bufferedSignals: sm.buffer } },
+          });
+        },
+      },
+    ],
+    inbound: [
+      {
+        async handleSignal(input, next) {
+          return await sm.wrapSignalHandler(input, next);
+        },
+        async execute(input, next) {
+          await next(input);
+          await wf.condition(() => wf.taskInfo().historyLength >= 2000);
+          await wf.continueAsNew<typeof someWorkflow>();
+        },
+      },
+    ],
+  };
+};
