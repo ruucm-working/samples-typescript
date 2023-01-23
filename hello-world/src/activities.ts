bb503a7d0c565534a929d7857538b6459a8b6bf5
ruucm-working <bumbermin@gmail.com>
2023-01-23 10:06:21 +0000

    test change

--- hello-world/src/activities.ts
+++ hello-world/src/activities.ts
@@ -1,5 +1,5 @@
 // @@@SNIPSTART typescript-hello-activity
 export async function greet(name: string): Promise<string> {
-  return `Hello, ${name}!`;
+  return `Hello@#!@#~~, ${name}!`;
 }
 // @@@SNIPEND
